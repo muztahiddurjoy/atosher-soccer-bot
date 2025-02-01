16c0f22576bf995355b489d6e9d1555c71fc8ea3
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2025-02-01 21:26:26 +0600

    changed

--- atosher_socker_bot.ino
+++ atosher_socker_bot.ino
@@ -5,10 +5,20 @@
 #define BT_RX 3
 #define BT_TX 2
 
+#define E1 4
+#define E2 7
+#define E3 8
+#define E4 11
+#define E5 12
+#define E6 13
+// #define E7 4
+// #define E8 4
+
+
 
 //Defining motors
-IBT left(4,5);
-IBT right(6,7);
+IBT left(5,6);
+IBT right(9,10);
 
 int speed = 50;
 
@@ -20,6 +30,15 @@ void setup() {
     
     pinMode(LED_PIN, OUTPUT);
     
+    pinMode(E1,OUTPUT);
+    pinMode(E2,OUTPUT);
+    pinMode(E3,OUTPUT);
+    pinMode(E4,OUTPUT);
+    pinMode(E5,OUTPUT);
+    pinMode(E6,OUTPUT);
+    
+
+
     Serial.println("Initializing...");
     Serial.println("Bluetooth ready for pairing.");
 }
@@ -27,32 +46,60 @@ void setup() {
 
 
 void loop() {
+  digitalWrite(E1,HIGH);
+  digitalWrite(E2,HIGH);
+  digitalWrite(E3,HIGH);
+  digitalWrite(E4,HIGH);
+  digitalWrite(E5,HIGH);
+  digitalWrite(E6,HIGH);
+  
     if (mySerial.available()) {
 
-        int command = mySerial.read();
-        
+        char command = mySerial.read();
+        if(command!='S'){
+          Serial.print("Received");
+          
+            Serial.println(command); // Print received byte in HEX format
+        }
         if(command>=48&&command<=57){
           int rawInt = command;
-          speed = map(rawInt,48,57,10,200);
+          speed = map(rawInt,48,57,10,255);
           Serial.println(speed);
         }
-        else if(command==70){
-          forward();
+
+        switch(command){
+          case 'F':
+            forward();
+            break;
+          case 'S':
+            stop();
+            break;
+          case 'B':
+            backward();
+            break;
+          case 'L':
+            leftward();
+            break;
+          case 'R':
+            rightward();
+            break;
+          
         }
-        else if(command=66){
-          backward();
-        }
-        else if(command==76){
-          leftward();
-        }
-        else if(command==82){
-          rightward();
-        }
-        if(command!=83){
-          Serial.print("Received");
-          int comInt = command;
-            Serial.println(comInt); // Print received byte in HEX format
-        }
+        // else if(command==70){
+        //   forward();
+        // }
+        // else if(command=66){
+        //   // backward();
+        // }
+        // else if(command==76){
+        //   leftward();
+        // }
+        // else if(command==82){
+        //   rightward();
+        // }
+        
+          
+        
         
     }
 
@@ -81,4 +128,9 @@ void rightward(){
   left.setRawSpeed(speed/5);
   right.setRawSpeed(-speed/5);
   Serial.println("going right");
+}
+
+void stop(){
+  left.setRawSpeed(0);
+  right.setRawSpeed(0);
 }
