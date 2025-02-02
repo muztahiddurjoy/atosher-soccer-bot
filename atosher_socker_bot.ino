f42db29e4f5df71255129e748ed4c1bff69f1b99
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2025-02-02 23:11:42 +0600

    flushed variable

--- atosher_socker_bot.ino
+++ atosher_socker_bot.ino
@@ -9,18 +9,16 @@
 #define E2 7
 #define E3 8
 #define E4 11
-#define E5 12
-#define E6 13
 // #define E7 4
 // #define E8 4
 
 
 
 //Defining motors
-IBT left(5,6);
+IBT left(5,6);  
 IBT right(9,10);
 
-int speed = 50;
+int speed = 150;
 
 SoftwareSerial mySerial(BT_RX, BT_TX); // Bluetooth module HC-05 on pins 3 (RX) & 2 (TX)
 
@@ -34,31 +32,22 @@ void setup() {
     pinMode(E2,OUTPUT);
     pinMode(E3,OUTPUT);
     pinMode(E4,OUTPUT);
-    pinMode(E5,OUTPUT);
-    pinMode(E6,OUTPUT);
     
-
-
     Serial.println("Initializing...");
     Serial.println("Bluetooth ready for pairing.");
+
+  digitalWrite(E1,HIGH);
+  digitalWrite(E2,HIGH);
+  digitalWrite(E3,HIGH);
+  digitalWrite(E4,HIGH);
 }
 
 
 
 void loop() {
-  digitalWrite(E1,HIGH);
-  digitalWrite(E2,HIGH);
-  digitalWrite(E3,HIGH);
-  digitalWrite(E4,HIGH);
-  digitalWrite(E5,HIGH);
-  digitalWrite(E6,HIGH);
-  
     if (mySerial.available()) {
-
         char command = mySerial.read();
         if(command!='S'){
-          Serial.print("Received");
-          
             Serial.println(command); // Print received byte in HEX format
         }
         if(command>=48&&command<=57){
@@ -83,51 +72,51 @@ void loop() {
           case 'R':
             rightward();
             break;
+          case 'I':
+            frontRight();
+            break;
+          case 'G':
+            frontLeft();
+            break;
           
         }
-        // else if(command==70){
-        //   forward();
-        // }
-        // else if(command=66){
-        //   // backward();
-        // }
-        // else if(command==76){
-        //   leftward();
-        // }
-        // else if(command==82){
-        //   rightward();
-        // }
         
-          
-        
-        
+
+        command = '\0';  // Reset after processing        
     }
+     
 
 }
 
 void forward(){
   left.setRawSpeed(speed);
   right.setRawSpeed(speed);
-  Serial.println("going forward");
 }
 
 
 void backward(){
   left.setRawSpeed(-speed);
   right.setRawSpeed(-speed);
-  Serial.println("going backward");
 }
 
 void leftward(){
-  left.setRawSpeed(-speed/5);
-  right.setRawSpeed(speed/5);
-  Serial.println("going left");
+  left.setRawSpeed(-speed);
+  right.setRawSpeed(speed);
+}
+
+void frontLeft(){
+  left.setRawSpeed(speed/2);
+  right.setRawSpeed(speed);
 }
 
 void rightward(){
-  left.setRawSpeed(speed/5);
-  right.setRawSpeed(-speed/5);
-  Serial.println("going right");
+  left.setRawSpeed(speed);
+  right.setRawSpeed(-speed);
+}
+
+void frontRight(){
+  left.setRawSpeed(speed);
+  right.setRawSpeed(speed/2);
 }
 
 void stop(){
